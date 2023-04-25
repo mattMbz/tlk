9033f0c9e343bf74edb0a8baa69d4da976db4289
mattmbz <developer@gmail.com>
2023-04-25 12:27:40 -0300

    Error control with try-except and added new features in Menu options

--- tlk/bash.py
+++ tlk/bash.py
@@ -3,18 +3,12 @@ import subprocess, os
 
 def welcome():
     print("TLK is an interface. You can use TLK to communicate python with Linux OS, through Bash, Ansible or gRPC")
-## end welcome
+## end welcome()
 
 
 def version():
-    print("version 0.1")
-## end version
-
-
-## Run bash files
-def executeAllPath(path):
-    pass
-## end executeAllPath
+    print("version 1.0.0")
+## end version()
 
 
 def executeFile(path, filename, *params):
@@ -24,7 +18,18 @@ def executeFile(path, filename, *params):
     for param in params:
         command.append(param)
 
-    print(f'Input command: {command}')
-    
-    subprocess.run(command)
-## end executeFile
+    try:
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        print(f'msg -> {result.stdout.decode()}')
+
+    except subprocess.CalledProcessError as error:
+        # catch error and show output code and error message
+        print(f"code -> Output code: {error.returncode}")
+        print(f"error_msg -> Error message: {error.stderr.decode()}")
+## end executeFile()
+
+
+## Run bash files
+def executeAllPath(path):
+    pass
+## end executeAllPath
--- tlk/custom_REPL.py
+++ tlk/custom_REPL.py
@@ -9,32 +9,35 @@ load_dotenv()
 PATH=os.getenv('PATH_TO_SCRIPT')
 # TEST_PATH=os.getenv('PATH_TO_TEST')
 
-header='''
+
+menu='''
 +====================================+
 |  Qubik Hypervisor version 1.0.0    |
 |  Author: Matias Barboza            |
++====================================+
+|  Menu options:                     |
++====================================+
+|  1. Create VM                      |
+|  2. Remove Vm                      |
+|  3. Rename Vm                      |   
+|  4. Monitor                        |
+|  5. Exit                           |
 +====================================+
 '''
 
 def custom_repl():
     while True:
-        print(header)
-        # Display the menu options
-        print("Menu options:")
-        print("1. Create VM")
-        print("2. Remove VM")
-        print("3. Rename VM")
-        print("4. Monitor")
-        print("5. Exit")
-    
+        print(menu)
+
         # Ask the user to input an option
         option = input("Enter an option: ")
         print()
     
         # Evaluate the user's input
         if option == "1":
-            print("Creatring new virtual machine")
+            print("Creating new virtual machine")
             vm_name=input('Input the name of new VM name >> ')
+            print()
             if(vm_name!='.abort'):
                 if parse_vm_name(vm_name):
                     executeFile(PATH, 'clone-vm.sh', 'debian11-vm', vm_name)
@@ -42,11 +45,16 @@ def custom_repl():
         elif option == "2":
             print("Removing virtual machine")
             vm_name=input('Input VM name to remove >> ')
+            print()
             if(vm_name!='.abort'):
                 executeFile(PATH, 'remove-vm.sh', vm_name)
         
         elif option == "3":
-            print("This feature is not implemented yet! =(")
+            print("This feature has not been implemented yet! =(")
+            vm_name=input('Input VM name>> ')
+            print()
+            if(vm_name!='.abort'):
+                executeFile(PATH, 'scr01.sh')
 
         elif option == "4":
             print("This feature is not implemented yet! =(")
@@ -64,13 +72,23 @@ def custom_repl():
             os.system('clear')
 ## End custom_repl
 
+
 def exit_repl(response):
     ex=True
+    default_regex = r"^(\s+)?$"
     response=response.lower()
-    if response=='n' or response =='no':
+    if re.match(default_regex, response):
         ex=False
+    else:
+        if response=='n' or response =='no':
+            ex=False
+        elif response=='y' or response == 'yes':
+            ex=True
+        else:
+            ex=False
+        
     return ex
-## End exit_repl()
+## End of exit_repl()
 
 
 def parse_vm_name(vm_name):
@@ -81,6 +99,13 @@ def parse_vm_name(vm_name):
 
     if re.match(regex, vm_name):
         response=True
-
+    else:
+        message(1)
     return response
-## End parse_vm_name()
+## End of parse_vm_name()
+
+
+def message(code):
+    if code==1:
+        print('Invalid name, try without simbols!')
+## End of parse_vm_name()
